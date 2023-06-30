23f8d90a83b932645871e4e9363afc887195b617
Denis Valitov <ssilverufa@yandex.ru>
2023-06-30 22:26:15 +0500

    Исправить BASE_URL

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://https://api.nxxtrx.nomoreparties.sbs';
+export const BASE_URL = 'https://api.nxxtrx.nomoreparties.sbs';
 
 export const registr = (password, email) => {
   return fetch(`${BASE_URL}/signup`, {

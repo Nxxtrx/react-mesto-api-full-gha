789f963018ad8dc2f9c765b4d9f75f30fd53bcf7
Denis Valitov <ssilverufa@yandex.ru>
2023-07-04 00:57:14 +0500

    fix: исправить ошибку после ревью

--- backend/middlwares/error.js
+++ backend/middlwares/error.js
@@ -1,9 +1,10 @@
 const errorHandler = (err, req, res, next) => {
   if (!err.statusCode) {
     res.status(500).send({ message: 'На сервере произошла ошибка' });
+  } else {
+    res.status(err.statusCode).send({ message: err.message });
+    next();
   }
-  res.status(err.statusCode).send({ message: err.message });
-  next();
 };
 
 module.exports = errorHandler;
